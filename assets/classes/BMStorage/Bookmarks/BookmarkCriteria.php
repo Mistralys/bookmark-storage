53189ce70af22970b7b771fb310cd29358bd5e5b
Mistralys <s.mordziol@gmail.com>
2024-01-05 10:27:40 +0100

    Added filtering by rating.

--- assets/classes/BMStorage/Bookmarks/BookmarkCriteria.php
+++ assets/classes/BMStorage/Bookmarks/BookmarkCriteria.php
@@ -9,11 +9,39 @@ use DBHelper_StatementBuilder_ValuesContainer;
 
 class BookmarkCriteria extends DBHelper_BaseFilterCriteria
 {
+    private int $ratingFrom = 0;
+    private int $ratingTo = 0;
+
+    public function selectRatingFrom(int $from) : self
+    {
+        $this->ratingFrom = $from;
+        return $this;
+    }
+
+    public function selectRatingTo(int $to) : self
+    {
+        $this->ratingTo = $to;
+        return $this;
+    }
+
+    protected function prepareQuery(): void
+    {
+        if($this->ratingTo > 0) {
+            $this->addWhere($this->statement(sprintf('{bm_rating} <= %s', $this->ratingTo)));
+        }
+
+        if($this->ratingFrom > 0) {
+            $this->addWhere($this->statement(sprintf('{bm_rating} >= %s', $this->ratingFrom)));
+        }
+    }
+
     protected function _registerJoins(): void
     {
     }
 
     protected function _registerStatementValues(DBHelper_StatementBuilder_ValuesContainer $container): void
     {
+        $container
+            ->field('{bm_rating}', BookmarkCollection::COL_RATING);
     }
 }
--- assets/classes/BMStorage/Bookmarks/BookmarkFilterSettings.php
+++ assets/classes/BMStorage/Bookmarks/BookmarkFilterSettings.php
@@ -6,17 +6,55 @@ namespace BMStorage\Bookmarks;
 
 use DBHelper_BaseFilterSettings;
 
+/**
+ * @property BookmarkCriteria $filters
+ */
 class BookmarkFilterSettings extends DBHelper_BaseFilterSettings
 {
     const SETTING_SEARCH = 'search';
+    const SETTING_RATING_FROM = 'rating_from';
+    const SETTING_RATING_TO = 'rating_to';
 
     protected function registerSettings(): void
     {
         $this->registerSetting(self::SETTING_SEARCH, t('Search'));
+        $this->registerSetting(self::SETTING_RATING_FROM, t('From rating'));
+        $this->registerSetting(self::SETTING_RATING_TO, t('To rating'));
+    }
+
+    public function inject_rating_from() : void
+    {
+        $el = $this->addElementText(self::SETTING_RATING_FROM);
+        $el->addFilterTrim();
+
+        $this->form->addRuleInteger($el, 0, 6);
+    }
+
+    public function inject_rating_to() : void
+    {
+        $el = $this->addElementText(self::SETTING_RATING_TO);
+        $el->addFilterTrim();
+
+        $this->form->addRuleInteger($el, 0, 6);
     }
 
     protected function _configureFilters(): void
     {
         $this->filters->setSearch($this->getSettingString(self::SETTING_SEARCH));
+
+        $this->configureRating();
+    }
+
+    private function configureRating() : void
+    {
+        $from = $this->getSettingInt(self::SETTING_RATING_FROM);
+        if($from > 0) {
+            $this->filters->selectRatingFrom($from);
+        }
+
+        $to = $this->getSettingInt(self::SETTING_RATING_TO);
+        if($to > 0) {
+            $this->filters->selectRatingTo($to);
+        }
     }
 }
